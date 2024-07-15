4d390d7a8f3945ed1f7fc409256a931d37207f57
Mark Omarov <15357910+mark-omarov@users.noreply.github.com>
2024-07-15 11:00:35 +0900

    feat: desktop two column list view (#31)
    Close #27

--- packages/app/src/components/url-list.tsx
+++ packages/app/src/components/url-list.tsx
@@ -1,15 +1,21 @@
 import { CopyURLButton } from '@/components/copy-url-button';
-import { Divider } from '@/components/divider';
 import { Input } from '@/components/ui/input';
 
 export const URLListItem = ({ url, ...props }: { url: string }) => {
   return (
     <li
-      className="space-y-2 p-2 min-[375px]:flex min-[375px]:items-center min-[375px]:justify-between min-[375px]:space-x-2 min-[375px]:space-y-0"
+      className="space-y-2 p-2 min-[375px]:flex min-[375px]:items-center min-[375px]:justify-between min-[375px]:space-y-0"
       {...props}
     >
-      <Input disabled value={url} />
-      <CopyURLButton url={url} />
+      <Input
+        className="min-[375px]:rounded-none min-[375px]:rounded-l-lg min-[375px]:border-r-0"
+        disabled
+        value={url}
+      />
+      <CopyURLButton
+        className="min-[375px]:border-l-1 min-[375px]:rounded-none min-[375px]:rounded-r-lg"
+        url={url}
+      />
     </li>
   );
 };
@@ -20,13 +26,12 @@ export const URLList = ({
   list: { id: number; shortUrl: string }[];
 }) => {
   return (
-    <ul className="w-full max-w-sm">
-      {list.map((item, idx) => (
-        <div key={item.id}>
-          {idx > 0 && <Divider />}
-          <URLListItem url={item.shortUrl} />
-        </div>
-      ))}
-    </ul>
+    <section className="container mx-auto max-w-3xl">
+      <ul className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        {list.map((item) => (
+          <URLListItem key={item.id} url={item.shortUrl} />
+        ))}
+      </ul>
+    </section>
   );
 };
