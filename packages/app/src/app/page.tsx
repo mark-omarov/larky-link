78392738300c3751f9f1ce5c811a6581beeca5bb
Mark Omarov <15357910+mark-omarov@users.noreply.github.com>
2024-07-15 10:56:40 +0900

    feat: pagination items per page selector

--- packages/app/src/app/page.tsx
+++ packages/app/src/app/page.tsx
@@ -3,7 +3,7 @@ import { env } from '@/env.mjs';
 import { getSessionKey } from '@/lib/session';
 import {
   fetchPaginatedURLs,
-  type URLSearchParams,
+  type URLServiceSearchParams,
   type URLPaginationResult,
 } from '@/services/urlService';
 import {
@@ -15,15 +15,19 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from '@/components/ui/pagination';
-import { getPageLinks } from '@/lib/utils';
+import { getPageLinks, getUrlWithUpdatedParams } from '@/lib/utils';
+import { ItemsPerPageSelect } from '@/components/items-per-page-select';
+
+export const ITEMS_PER_PAGE_OPTIONS = ['10', '30', '50'] as const;
 
 // TODO: Separate and setup error boundary
 export default async function Home({
-  searchParams,
+  searchParams: searchParamsProp,
 }: {
-  searchParams: URLSearchParams;
+  searchParams: URLServiceSearchParams;
 }) {
   const sessionKey = getSessionKey();
+  const searchParams = sanitizeSearchParams(searchParamsProp);
   const urlPaginationResult: URLPaginationResult | null = sessionKey
     ? await fetchPaginatedURLs({ searchParams, sessionKey })
     : null;
@@ -43,33 +47,72 @@ export default async function Home({
           shortUrl: new URL(item.key, env.DOMAIN).toString(),
         }))}
       />
-      <Pagination>
-        <PaginationContent>
-          {page > 1 && (
-            <PaginationItem>
-              <PaginationPrevious href={`?page=${page - 1}`} />
-            </PaginationItem>
-          )}
-          {pageLinks.map((p, index) =>
-            p === '...' ? (
-              <PaginationItem key={index}>
-                <PaginationEllipsis />
+      <footer className="container mx-auto max-w-3xl">
+        <section className="mx-auto flex w-full justify-end">
+          <ItemsPerPageSelect
+            value={searchParams.limit}
+            options={ITEMS_PER_PAGE_OPTIONS}
+          />
+        </section>
+        <Pagination>
+          <PaginationContent>
+            {page > 1 && (
+              <PaginationItem>
+                <PaginationPrevious
+                  href={`?${getUrlWithUpdatedParams(searchParams, {
+                    page: page - 1,
+                  }).toString()}`}
+                />
               </PaginationItem>
-            ) : (
-              <PaginationItem key={index}>
-                <PaginationLink isActive={p === page} href={`?page=${p}`}>
-                  {p}
-                </PaginationLink>
+            )}
+            {pageLinks.map((p, idx) =>
+              p === '...' ? (
+                <PaginationItem key={idx}>
+                  <PaginationEllipsis />
+                </PaginationItem>
+              ) : (
+                <PaginationItem key={idx}>
+                  <PaginationLink
+                    isActive={p === page}
+                    href={`?${getUrlWithUpdatedParams(searchParams, {
+                      page: p,
+                    }).toString()}`}
+                  >
+                    {p}
+                  </PaginationLink>
+                </PaginationItem>
+              ),
+            )}
+            {page < pages && (
+              <PaginationItem>
+                <PaginationNext
+                  href={`?${getUrlWithUpdatedParams(searchParams, {
+                    page: page + 1,
+                  }).toString()}`}
+                />
               </PaginationItem>
-            ),
-          )}
-          {page < pages && (
-            <PaginationItem>
-              <PaginationNext href={`?page=${page + 1}`} />
-            </PaginationItem>
-          )}
-        </PaginationContent>
-      </Pagination>
+            )}
+          </PaginationContent>
+        </Pagination>
+      </footer>
     </main>
   );
+}
+
+function sanitizeSearchParams(
+  searchParams: Partial<URLServiceSearchParams>,
+): URLServiceSearchParams {
+  const urlSearchParams = new URLSearchParams(searchParams);
+  if (
+    !ITEMS_PER_PAGE_OPTIONS.some(
+      (option) => option === urlSearchParams.get('limit'),
+    )
+  ) {
+    urlSearchParams.set('limit', ITEMS_PER_PAGE_OPTIONS[0]);
+  }
+
+  return {
+    limit: urlSearchParams.get('limit') ?? ITEMS_PER_PAGE_OPTIONS[0],
+    page: urlSearchParams.get('page') ?? '1',
+  };
 }
---
+++ packages/app/src/components/items-per-page-select.tsx
@@ -0,0 +1,41 @@
+'use client';
+import { useRouter, useSearchParams } from 'next/navigation';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+export const ItemsPerPageSelect = ({
+  value,
+  options,
+}: {
+  value?: string;
+  options: Readonly<string[]>;
+}) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleChange = (value: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('limit', value);
+    router.push(`${window.location.pathname}?${params.toString()}`);
+  };
+
+  return (
+    <Select value={value} onValueChange={handleChange}>
+      <SelectTrigger className="w-[150px]">
+        <SelectValue placeholder="Items per page" />
+      </SelectTrigger>
+      <SelectContent>
+        {options.map((option) => (
+          <SelectItem key={option} value={option}>
+            {option}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
