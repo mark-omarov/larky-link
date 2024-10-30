9a2badb7e5b07f1b78a0e71cbba4d4e1fceebdbb
Mark Omarov <15357910+mark-omarov@users.noreply.github.com>
2024-10-30 18:20:22 +0900

    fix: reuse pg connection on dev

--- packages/app/src/connectors/postgres.ts
+++ packages/app/src/connectors/postgres.ts
@@ -2,6 +2,10 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { env } from '@/env.mjs';
 
+let globalForClient = global as typeof global & {
+  postgresClient?: ReturnType<typeof postgres>;
+};
+
 /**
  * No need to manually close the connection when running in serverless environments like Vercel.
  * @see {@link https://github.com/porsager/postgres?tab=readme-ov-file#connection-timeout|Postgres} Docs
@@ -9,5 +13,14 @@ import { env } from '@/env.mjs';
  * Disable prefetch as it is not supported for "Transaction" pool mode.
  * @see {@link https://supabase.com/docs/guides/database/connecting-to-postgres#connecting-with-drizzle|Supabase} Docs
  */
-export const client = postgres(env.DATABASE_URL, { prepare: false });
+export const client =
+  globalForClient.postgresClient ??
+  postgres(env.DATABASE_URL, {
+    prepare: false,
+    idle_timeout: 5,
+  });
+
+if (process.env.NODE_ENV !== 'production')
+  globalForClient.postgresClient = client;
+
 export const pg = drizzle(client);
